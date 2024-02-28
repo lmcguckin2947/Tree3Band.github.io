f4dcfc52c075b88d5faa1749759969f0c185f133
Liam McGuckin <mcgucklm@miamioh.edu>
2024-02-28 00:42:53 -0500

    Included dynamic gig option.

---
+++ gigs.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    fetch("gigs.json")
+        .then(response => response.json())
+        .then(response => response.forEach(parseGigs));
+  });
+
+function parseGigs(gig) {
+    let date = new Date(gig.Date);
+    let today = new Date();
+    if (today.getTime() <= date.getTime()) {
+        $(".wrapper").append(
+            `<div class="item">
+                    <div class="card" style="width: 18rem; background-color:#fefae0">
+                        <div class="card-body">
+                            <h5 class="card-title">${gig.Title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${date.toDateString()}, ${gig.Time}</h6>
+                            <p class="card-text">${gig.Description}</p>
+                            <a href="${gig.LocationURL}" class="card-link">Directions</a>
+                            <a href="${gig.WebsiteURL}" class="card-link">Website Link</a>
+                        </div>
+                    </div>
+                </div>`
+        )
+    }
+}
